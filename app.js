f470902904ff24ba1e5163fb068ebe3a31fdd68f
yyellen <yyopenfolder@gmail.com>
2022-11-25 17:26:20 +0800

    refactor: 產生 todo item 物件，放進 todoItems 陣列中

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const createButton = document.querySelector(".create-button");
 const input = document.querySelector(".todo-input");
 
-const createTodoItem = () => {
-  const inputValue = input.value;
-  const todoItem = generateTodoItem(inputValue);
+const todoItems = [];
 
-  console.log(todoItem);
+const createTodoItem = () => {
+  // 拿到輸入框裡的值
+  const inputValue = input.value;
+  // 透過 createTodoItemData 產生 todo item 物件
+  const todoItem = createTodoItemData(inputValue);
+
+  // 拿到todo item 物件後，放進 todoItems 陣列中
+  todoItems.push(todoItem);
+  console.log(todoItems);
 };
+
+createButton.addEventListener("click", createTodoItem);
 
 const generateTodoItem = (text) => {
   const container = document.createElement("div");
@@ -21,4 +29,10 @@ const generateTodoItem = (text) => {
   return container;
 };
 
-createButton.addEventListener("click", createTodoItem);
+const createTodoItemData = (text) => {
+  const data = {
+    text: text
+  };
+
+  return data;
+}
